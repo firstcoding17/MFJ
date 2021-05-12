bc2af747f4fa46c7751434b612e1c959f2594fca
whitewatch(하얀손목시계) <39446151+firstcoding17@users.noreply.github.com>
2021-05-12 22:17:47 +0900

    Add files via upload

--- GUI/Main.py
+++ GUI/Main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget
 
 class MyApp(QWidget):
 
@@ -9,10 +9,28 @@ class MyApp(QWidget):
 
     #너비 1200 넓이 800
     def initUI(self):
+        label1 = QLabel('기능1', self)
+        label2 = QLabel('기능2', self)
+        label1.move(0,200)
+        label2.move(10,30)
+
+        Loding = QPushButton('Loding',self)
+        Loding.setToolTip('csv파일 불러오기')
+        Loding.move(0,0)
+        Loding.resize(50,30)
+
+        save = QPushButton('save',self)
+        save.setToolTip('현재 csv 상황 저장')
+        save.move(0,30)
+        save.resize(50,30)
+
+
         self.setWindowTitle('My First journey')
         self.move(0, 0)
         self.resize(1200, 800)
+
         self.show()
+
 
 
 if __name__ == '__main__':
